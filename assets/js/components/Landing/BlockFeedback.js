ea1224e58fa5668b0a64b5600f2655cbdf315030
dimmy_raves <54665734+skvortsov618@users.noreply.github.com>
2021-12-01 16:47:40 +0300

    added formik  and it's working
    basic form in blockfeedback works with mui+formik

--- assets/js/components/Landing/BlockFeedback.js
+++ assets/js/components/Landing/BlockFeedback.js
@@ -1,67 +1,75 @@
 import React from "react";
 import styled from "styled-components";
-import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import { Field, Form, Formik } from "formik";
+import { LinearProgress } from "@mui/material";
+
+import MuiTextField from '@mui/material/TextField';
+import { Autocomplete,
+    TextField,
+    Select,
+    Switch,
+    ToggleButtonGroup,} from "formik-mui";
 
 const BlockFeedback = () => {
 
-    const image_src = "https://images7.alphacoders.com/451/451013.jpg";
-    const image_alt = "sdfsd";
-    const header    = "Lokirel";
-    const subheader = "Озеленение и профессиональный уход за растениями";
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(e)
-        //TODO add validation
-        //TODO make json and send to backend
-        //TODO change form to "form submitted"
-    }
-    const SIF = styled.div`
-        background-color: white;`
+    const Feedback = styled.div`
+        background-color: lightblue`
 
     return ( 
-        <div className="blockFeedback" style={{
-            width: "100%",
-            height: "100vh",
-            backgroundColor: "white"
-        }}>
-            <h2 className="blockFeedback__title" style= {{
-                textAlign: "center",
-                color: "black", fontSize: "50px"
-            }}>CONFIDE IN US</h2>
-            <div id="formholder">
-                <div id="afterform"></div>
-                <form className="blockFeedback__form" autoComplete="on" style={{
-                    backgroundColor: "lightgrey",
-                    border: "2px lightgrey solid",
-                    borderRadius: 20, margin: 100, padding: 30
-                }}>
-                    <SIF>
-                    <TextField
-                        required
-                        fullWidth
-                        id="lastName"
-                        label="Last Name"
-                        name="lastName"
-                        autoComplete="family-name"
-                    />
-                    </SIF>
-                    <input type="text" id="fname" name="fname" maxLength="50" placeholder="Как к вам обращаться?"/><br />
-                    <fieldset>
-                        <legend>Контакты</legend>
-                        <input type="email" id="femail" name="femail" maxLength="50" placeholder="email" /><br />
-                        <div id="femail_error"></div>
-                        <input type="tel" id="ftel" name="ftel" maxLength="50" placeholder="Телефон" /><br />
-                        <div id="ftel_error"></div>
-                    </fieldset>
-                    <textarea rows="10" cols="30" id="ftext" name="ftext" maxLength="220" placeholder="Дополнительное сообщение" /><br />
-                    <input type="checkbox" id="fcheck" name="fcheck" required/>
-                    <label htmlFor="fcheck">Принимаю условия пользовательского соглашения и политики конфиденциальности</label><br />
-                    <div id="fcheck_error"></div>
-                    <input type="submit" onClick={handleSubmit} value="Submet" />
-                </form>
-            </div>
-        </div>
+        <Feedback>
+            <Formik
+                initialValues={{
+                    femail: "",
+                }}
+                validate={(values)=>{
+                    const errors = {};
+                    // if (!values.femail) {
+                    //     errors.femail = 'Required'
+                    // }
+                    return errors;
+                }}
+                onSubmit={(values, {setSubmitting}) =>{
+                    setTimeout(()=>{
+                        setSubmitting(false);
+                        alert(JSON.stringify(values, null, 2));
+                    }, 5000)
+                }}
+            >
+                {({submitForm, isSubmitting, touched, errors}) => (
+                    <Form style={{margin:100}}>
+                        <Field
+                            component={TextField}
+                            name="femail"
+                            label="email"
+                            type="email"
+                            variant="outlined"
+                            fullWidth
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        {isSubmitting && <LinearProgress />}
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            disabled={isSubmitting}
+                            onClick={submitForm}
+                        >
+                            Sign In
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Feedback>    
     );
 }
  
