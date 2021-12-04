c6e268d2d18d952d41cd5c047fceba9be45858c3
THEiDiet <alex96kravets@gmail.com>
2021-12-05 01:59:35 +0700

    чутьчуть футера

--- assets/js/components/Landing/BlockFooter.js
+++ assets/js/components/Landing/BlockFooter.js
@@ -1,38 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Divider from '@mui/material/Divider';
-import Stack from '@mui/material/Stack';
 import Container from '@mui/material/Container';
-import  './../../../styles/index.scss'
+import './../../../styles/index.scss'
+import Grid from "@mui/material/Grid";
 
 const BlockFooter = () => {
-
-    const image_src = "https://images7.alphacoders.com/451/451013.jpg";
-    const image_alt = "sdfsd";
-    const header    = "WELCOME";
-    const subheader = "To the bottom! Yeeey you did it!";
-
-    return ( 
-        <div className="blockCover" style={{
-            backgroundImage: `url(${image_src})`
-        }}>
-            <Stack sx={{width:"100%", height: "100%", backgroundColor: "rgba(0,0,0,0.5"}}>
-                <Container sx={{ width:"100%", height: "80%"}}>
-                    <Stack>
-                        <Link to="/contacts">Контакты</Link>
-                        <Link to="/privacypolicy">Политика конфиденциальности</Link>
-                    </Stack>
-                    <Stack>
-                        <div>Позвоните нам: <a href="tel:+79776340878">89776340878</a></div>
-                        <div>Напишите нам: <a href="https://wa.me/79776340878">WA</a></div>
-                        <div>Lokirel в соцсетях: <a>Inst</a><a>Facebook</a></div>
-                    </Stack>
-                </Container>
-                <Divider variant="middle" />
-                <Container sx={{ width:"100%", height: "20%", textAlign: "center"}}>LOKIREL 2021</Container>
-            </Stack>
-        </div>
-    );
+	
+	const image_src = "https://images7.alphacoders.com/451/451013.jpg";
+	const image_alt = "sdfsd";
+	const header = "WELCOME";
+	const subheader = "To the bottom! Yeeey you did it!";
+	
+	return (
+			<div className='blockCardPicture' style={{
+				backgroundImage: `url(${image_src})`
+			}}>
+			<Container sx={{color: 'black', paddingTop:'30vh', display:'flex', flexDirection: 'column', justifyContent:'center', alignItems:'center'}} className="blockCover" >
+				
+				<Grid container columns={{xs: 4, md: 12}} spacing={2} sx={{width:'80%', flex: '1 1 auto'}} >
+                        <Grid item md={6}>
+                            <div><Link   to="/contacts">Контакты</Link></div>
+                            <Link to="/privacypolicy">Политика конфиденциальности</Link>
+                        </Grid>
+                        <Grid item md={6} sx={{textAlign:'right'}}>
+                            <div>Позвоните нам: <a href="tel:+79776340878">89776340878</a></div>
+                            <div>Напишите нам: <a href="https://wa.me/79776340878">WA</a></div>
+                            <div>Lokirel в соцсетях: <a>Inst</a><a>Facebook</a></div>
+                        </Grid>
+				</Grid>
+				<Divider variant="middle" flexItem={true}/>
+				<Container sx={{ width:"100%", height: "20%", textAlign: "center", flex: ' 0 0 20%', padding:'3vh'}}>LOKIREL 2021</Container>
+			</Container>
+			</div>
+	);
 }
- 
+
 export default BlockFooter;
